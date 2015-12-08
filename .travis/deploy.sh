a2c1673683d1135023cbc3f3a2c25707e0185fcf
Christopher Métrailler <metc@users.noreply.github.com>
2015-12-08 12:54:00 +0100

    Website content added. Fix html frame size. Updated travis to build only from develop branch.

---
+++ .travis/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script updated from https://github.com/felixrieseberg/travis-jekyll-git/blob/master/scripts/deploy.sh
+if [ "$TRAVIS_REPO_SLUG" == "Electrical-Age/electrical-age.github.io" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]
+then
+
+  echo -e "Publishing latest website to 'http://electrical-age.github.io'\n"
+  cd _site
+
+  git init
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+
+  git add .
+  git commit -m $'Lastest website auto-published.\nTravis build '$TRAVIS_BUILD_NUMBER'.'
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
+
+else
+  echo -e "Skipping publishing...\n"
+fi
